7cea9e4289623f8800b40aa0f4ae14f988c4f387
hafiszikri28 <130953274+hafiszikri28@users.noreply.github.com>
2023-04-27 00:17:33 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,18 @@
-'''
-	AIM4 - Flask - [A] Basic - [01] First Time
-	
-	Orbit Future Academy - AI Mastery - KM Batch 4
-	Tim Deployment
-	2023
-'''
-
-# =[Modules dan Packages]========================
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 from flask_ngrok import run_with_ngrok
 
-# =[Variabel Global]=============================
 app = Flask(__name__)
 
-# =[Routing]=====================================
-@app.route("/")
-def beranda():
-    return "Halo Dunia ! Belajar AI di Orbit Future Academy"
 
-# =[Main]========================================
+@app.route("/", methods=['GET', 'POST'])
+def iris_prediction():
+    if request.method == 'GET':
+        return render_template("iris-prediction.html")
+    else:
+        print(request)
+        return jsonify(request.args)
+
+
 if __name__ == '__main__':
     run_with_ngrok(app)
     app.run()
